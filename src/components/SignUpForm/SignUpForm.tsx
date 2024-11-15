10e851218da9d78a39df6e99b8d6e3e12ad735b9
nicolasgcode <contactnicolasgarcia@gmail.com>
2024-11-15 00:40:33 -0300

    Rework some styles

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,5 @@
-
 import { SignUpFormProps } from '../../types';
-
-import styles from './SignUp.module.css'
+import styles from './SignUp.module.css';
 
 function SignUpForm({
   register,
@@ -14,65 +12,82 @@ function SignUpForm({
 }: SignUpFormProps) {
   return (
     <div className={styles.container}>
-    <h2>{user ? 'Edit user' : 'Create Account'}</h2>
-    <form onSubmit={onSubmit} className={styles.form}>
-      
-      <div>
-        <label htmlFor="dni">DNI</label>
-        <input
-          type="number"
-          {...register("dni")}
-          className={styles.input}
-        />
-        {errors.dni && <p style={{ color: 'red' }}>{errors.dni.message}</p>}
-      </div>
-       <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="string"
-          {...register("name")}
-          className={styles.input}
-        />
-        {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
-      </div>
-       <div>
-        <label htmlFor="surname">Surname</label>
-        <input
-          type="string"
-          {...register("surname")}
-          className={styles.input}
-        />
-        {errors.surname && <p style={{ color: 'red' }}>{errors.surname.message}</p>}
-      </div>
-       <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="string"
-          {...register("email")}
-          className={styles.input}
-        />
-        {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
-      </div>
-       <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          {...register("password")}
-          className={styles.input}
-        />
-        {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
-      </div>
-      
-      <button type="submit" className={styles.btn}>{user ? 'Save changes' : 'Sign Up'}</button>
-      {user && <button onClick={handleCancelEdit} className={styles.btn}>Cancel Edit</button> }
-      
-    </form>
-     {success && <p className={styles.success}>{success}</p>}
-     {error && <p className={styles.error}>{error}</p>}
-  
+      <h2>{user ? 'Edit user' : 'Create Account'}</h2>
+      <form onSubmit={onSubmit} className={styles.form}>
+        
+        <div className={styles.formgroup}>
+          <label htmlFor="dni">DNI:</label>
+          <input
+            type="number"
+            id="dni"
+            min="0"
+            {...register("dni")}
+            className={styles.input}
+          />
+          {errors.dni && <p style={{ color: 'red' }}>{errors.dni.message}</p>}
+        </div>
+
+        <div className={styles.formgroup}>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            {...register("name")}
+            className={styles.input}
+          />
+          {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
+        </div>
+
+        <div className={styles.formgroup}>
+          <label htmlFor="surname">Surname:</label>
+          <input
+            type="text"
+            id="surname"
+            {...register("surname")}
+            className={styles.input}
+          />
+          {errors.surname && <p style={{ color: 'red' }}>{errors.surname.message}</p>}
+        </div>
+
+        <div className={styles.formgroup}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email")}
+            className={styles.input}
+          />
+          {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+        </div>
+
+        <div className={styles.formgroup}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            {...register("password")}
+            className={styles.input}
+          />
+          {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
+        </div>
+
+        <button type="submit" className={styles.btn}>
+          {user ? 'Save changes' : 'Sign Up'}
+        </button>
+
+        {user && (
+          <button onClick={handleCancelEdit} className={styles.btn}>
+            Cancel Edit
+          </button>
+        )}
+
+        {success && <p className={styles.success}>{success}</p>}
+        {error && <p className={styles.error}>{error}</p>}
+      </form>
     </div>
   );
 }
 
 export default SignUpForm;
+
 
